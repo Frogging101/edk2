d0cf1f35cba99200689b8c72273d1a5404c4b64a
John Brooks <john@fastquake.com>
2016-01-24 14:37:46 -0500

    replace.sh

---
+++ replace.sh
@@ -0,0 +1,4 @@
+sudo cp Build/OvmfX64/DEBUG_GCC48/FV/OVMF_CODE.fd /usr/share/OVMF/OVMF_CODE.fd
+sudo cp Build/OvmfX64/DEBUG_GCC48/FV/OVMF_VARS.fd /usr/share/OVMF/OVMF_VARS.fd
+sudo cp Build/OvmfX64/DEBUG_GCC48/FV/OVMF.fd /usr/share/ovmf/
+sudo cp Build/OvmfX64/DEBUG_GCC48/FV/OVMF.fd /usr/share/qemu/
